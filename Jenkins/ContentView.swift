661390bfe40d24dd88dc986f452a8ce96278f472
Kudinov Ivan <62946157+medievil-inc@users.noreply.github.com>
2022-01-18 15:18:51 +0300

    Initial Commit

---
+++ Jenkins/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Jenkins
+//
+//  Created by Ivan Kudinov on 18/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Jenkins/JenkinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JenkinsApp.swift
+//  Jenkins
+//
+//  Created by Ivan Kudinov on 18/1/22.
+//
+
+import SwiftUI
+
+@main
+struct JenkinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
